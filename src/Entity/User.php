680ce546fcf93f300aa221116259b5577442c92f
Hamza Hajjaji <hh.hajjaji.hamza@gmail.com>
2026-01-12 15:24:41 +0100

    feat(entities): ajouter les entités Role et User
    - Création de l'entité Role avec les propriétés id et name
    - Ajout des méthodes getter et setter pour l'entité Role
    - Création de l'entité User avec les propriétés id, name, email, password et role
    - Implémentation des méthodes getter et setter pour l'entité User
    - Ajout du constructeur avec injection des dépendances pour les deux entités
    - Mise en place de la relation entre User et Role via la propriété role

---
+++ src/Entity/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace src\Entity;
+
+class User
+{
+    private ?int $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    private Role $role;
+
+    public function __construct(?int $id, string $name, string $email, string $password, Role $role)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getRole(): Role
+    {
+        return $this->role;
+    }
+
+    public function setRole(Role $role): self
+    {
+        $this->role = $role;
+        return $this;
+    }
+}
